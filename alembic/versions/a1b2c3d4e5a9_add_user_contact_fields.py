2a327382e8777e1c566e20743d9ff5a3888a243f
sandipmaurya2611 <mauryasandip4you@gmail.com>
2025-12-18 07:51:30 +0530

    Project property new alembic

--- alembic/versions/a1b2c3d4e5a9_add_user_contact_fields.py
+++ alembic/versions/a1b2c3d4e5a9_add_user_contact_fields.py
@@ -1,7 +1,7 @@
 """add contact gender address to users
 
-Revision ID: a1b2c3d4e5a8
-Revises: a1b2c3d4e5a7
+Revision ID: a1b2c3d4e5a9
+Revises: a1b2c3d4e5a8
 Create Date: 2025-12-16 11:30:00.000000
 
 """
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a1b2c3d4e5a8'
-down_revision = 'a1b2c3d4e5a7'
+revision = 'a1b2c3d4e5a9'
+down_revision = 'a1b2c3d4e5a8'
 branch_labels = None
 depends_on = None
 
